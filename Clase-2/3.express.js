4d46168107d9773f9fe3f8cea99b7d4369cd8d66
Aaron Guzman <aaronguf.93@hotmail.com>
2024-06-26 17:38:53 -0600

    My first API with express framework

--- Clase-2/3.express.js
+++ Clase-2/3.express.js
@@ -1,12 +1,34 @@
 const express = require('express')
+const ditto = require('./pokemon/ditto.json')
+
 const app = express()
+app.disable('X-Powered-By')
 
 const PORT = process.env.PORT ?? 1234
 
-app.get('/', (req, res) => {
-  res.status(200).send('<h1>Mi página</h1>')
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
 })
 
-spp.listen(PORT, () => {
-  console.log(`server listening on port https://localhost:${PORT}`)
+app.post('/pokemon', (req, res) => {
+  let body = ''
+
+  // escuchar el evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    // llamar a una base de datos para guardar la info
+    res.status(201).json(data)
+  })
+})
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
 })
