e1aacaa87b5c7f1efa8bcde76bd21fcaed507feb
Aaron Guzman <aaronguf.93@hotmail.com>
2024-06-26 18:17:00 -0600

    using express.json and going inside this function to see how it is working

--- Clase-2/3.express.js
+++ Clase-2/3.express.js
@@ -1,28 +1,39 @@
 const express = require('express')
 const ditto = require('./pokemon/ditto.json')
 
-const app = express()
-app.disable('X-Powered-By')
-
 const PORT = process.env.PORT ?? 1234
+
+const app = express()
+app.disable('x-Powered-by')
+
+app.use(express.json())
+
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   // solo llega request que son POSt y que tienen el header Content-Type: application/json
+//   let body = ''
+
+//   // escuchar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     // mutar la equiesr y meter la indomación en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
 
 app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
 })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
-
-  // escuchar el evento data
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    // llamar a una base de datos para guardar la info
-    res.status(201).json(data)
-  })
+  res.status(201).json(req.body)
 })
 
 app.use((req, res) => {
